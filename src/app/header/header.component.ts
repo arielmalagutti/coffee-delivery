cb643a90adbe633d0472974b999ed75688f86c49
Ariel <arielmalagutti.job@gmail.com>
2023-12-11 00:36:57 -0300

    Component - Create Header

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { NgxPhosphorIconsModule } from 'ngx-phosphor-icons';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatIconModule, NgxPhosphorIconsModule],
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { NgxPhosphorIconsModule } from 'ngx-phosphor-icons';
 
+import { HeaderComponent } from '../header/header.component';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgxPhosphorIconsModule, MatIconModule],
+  imports: [NgxPhosphorIconsModule, MatIconModule, HeaderComponent],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {}
