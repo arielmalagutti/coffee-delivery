bccfe99bf3a1c9ac8b95f550ba9abd2e32203d7e
Ariel <arielmalagutti.job@gmail.com>
2023-12-11 13:01:25 -0300

    Component - Add mock of numbers in cart in Header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { NgxPhosphorIconsModule } from 'ngx-phosphor-icons';
 
@@ -8,4 +8,6 @@ import { NgxPhosphorIconsModule } from 'ngx-phosphor-icons';
   imports: [MatIconModule, NgxPhosphorIconsModule],
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Input() cart = { items: [] };
+}
